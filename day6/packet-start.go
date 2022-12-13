92e453ba00b7b8dcf39f8ddcfa89dc9a9d8e1d36
sanjana <4946240+s4swani@users.noreply.github.com>
2022-12-14 04:20:31 +0530

    code cleanup

--- day6/packet-start.go
+++ day6/packet-start.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	//	"bufio"
 	"strings"
 	"io/ioutil"
 	"log"
@@ -16,10 +15,6 @@ func isError(err error) bool {
 	}
 
 	return (err != nil)
-}
-
-func not_same1(char_1 uint8, char_2 uint8) bool {
-	return (char_1 != char_2)
 }
 
 // function to recursively check for dupes
@@ -51,36 +46,13 @@ func get_start(ip_file []uint8, packet_length int) int {
 
 
 func main() {
-	ip_file, err := ioutil.ReadFile("ip-file")
 	var packet_start, message_start int = 0, 0
-	//	var charA, charB, charC, charD uint8
-	
+		
+	ip_file, err := ioutil.ReadFile("ip-file")	
+
 	if isError(err) {
 		return
 	}
-
-	// charA = ip_file[0]
-	// charB = ip_file[1]
-	// charC = ip_file[2]
-
-	// for i := 3; i < len(ip_file); i++ {
-		
-	// 	charD = ip_file[i]
-
-	// 	if (not_same(charA, charB) &&
-	// 		not_same(charA, charC) &&
-	// 		not_same(charA, charD) &&
-	// 		not_same(charB, charC) &&
-	// 		not_same(charB, charD) &&
-	// 		not_same(charC, charD)) {
-	// 		break;
-	// 	} else {
-	// 		packet_start++
-	// 		charA = charB
-	// 		charB = charC
-	// 		charC = charD
-	// 	}
-	// }
 
 	packet_start = get_start(ip_file, 4)
 
@@ -89,5 +61,4 @@ func main() {
 	fmt.Printf("Start of packet = %d\n", packet_start)
 
 	fmt.Printf("Start of message = %d\n", message_start)
-	
 }
